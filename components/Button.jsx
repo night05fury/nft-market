1278e8133e5c3403a2c9a5c2c7e0c02e977c01ac
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2024-12-11 20:07:29 +0530

    feat: Enhance Button component with dynamic styles and click handling

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+const primaryBtn = 'nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8 font-poppins font-semibold text-white';
+const outlineBtn = 'border border-nft-red-violet bg-transparent font-poppins font-semibold text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8';
 
-const Button = () => 
-  (
-    <div>Button</div>
-  )
+const Button = ({ btnName, classStyles, btnType, handleClick }) => (
+  <button
+    type="button"
+    className={
+        btnType === 'primary' ? `${primaryBtn} ${classStyles}` : `${outlineBtn} ${classStyles}`
+    }
+    onClick={handleClick}
+  >
+    {btnName}
+  </button>
+);
 
-
-export default Button
+export default Button;
