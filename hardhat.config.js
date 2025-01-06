477410d85357d28f087a51f50107a3f39def9d7c
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2025-01-06 22:24:58 +0530

    feat: Update Hardhat configuration to include Mumbai testnet and secure private key management

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,22 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+
+require('@nomiclabs/hardhat-waffle');
+// Read the private key securely from a file named '.secret'
+const privateKey = fs.readFileSync('.secret').toString().trim();
+console,.log(privateKey);
+
 module.exports = {
-  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+     // Mumbai testnet for Polygon network ('mumbai')
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [privateKey],
+    },
+
+  },
+  solidity: '0.8.4',
 };
+
